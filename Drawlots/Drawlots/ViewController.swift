455c631afa5d742d453ed8dc0d708bbab54bf5a3
Georgehu <georgehu@GeorgehudeMacBook-Pro.local>
2019-01-07 19:34:16 +0800

    first commit

---
+++ Drawlots/Drawlots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drawlots
+//
+//  Created by Georgehu on 2019/1/7.
+//  Copyright © 2019年 Georgehu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
