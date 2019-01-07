afe5622ed531b522a10ac175775e92b85b96126b
林家辉 <fcxl9876@linjiahuideMacBook-Pro.local>
2019-01-07 21:12:10 +0800

    add exit

--- Drawlots/Drawlots/ViewController.swift
+++ Drawlots/Drawlots/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func exitToHere(sender: UIStoryboardSegue) {
+        // No code needed!
+    }
 
 }
 
