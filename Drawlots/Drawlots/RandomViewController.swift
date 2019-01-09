6f289634c84150f496f56b5eadd41af3faebbdd3
Georgehu <georgehu@GeorgehudeMacBook-Pro.local>
2019-01-09 16:18:22 +0800

    update random

--- Drawlots/Drawlots/RandomViewController.swift
+++ Drawlots/Drawlots/RandomViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class RandomViewController: UIViewController {
 
+    var flag: Bool = true
     @IBOutlet weak var start: UITextField!
     @IBOutlet weak var end: UITextField!
     @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var startBtn: UIButton!
     
     @IBAction func random(_ sender: Any) {
          result.text = String(Int(arc4random()) % (Int(end.text!)! - Int(start.text!)! + 1) + Int(start.text!)!)
     }
 
-    /*
-    func randomIn(min: Int, max: Int) -> Int {
-        return Int(arc4random()) % (max - min + 1) + min
+    //开始按钮
+    @IBAction func startBtn(_ sender: Any) {
+        //var timer = Timer()
+        if(flag)
+        {
+            startBtn.setTitle("停止",for: .normal)
+            // 启用计时器，控制每秒执行一次tickDown方法
+            //timer = Timer.scheduledTimer(timeInterval: 0.5,target:self,selector:Selector(("random")),userInfo:nil,repeats:true)
+            
+        }
+        else
+        {
+            startBtn.setTitle("开始",for: .normal)
+            //timer.invalidate()
+        }
     }
-    */
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
